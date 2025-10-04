beea70a89188cd2cb6028e70a66198bd5a3cc53a
Shahwatrajsingh <shashwatrajsing@gmail.com>
2025-10-04 15:20:10 +0530

    deploy

--- client/src/components/ui/card.js
+++ client/src/components/ui/card.js
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
+const CardTitle = React.forwardRef(({ className, children, ...props }, ref) => (
   <h3
     ref={ref}
     className={cn(
@@ -31,7 +31,9 @@ const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
       className
     )}
     {...props}
-  />
+  >
+    {children}
+  </h3>
 ))
 CardTitle.displayName = "CardTitle"
 
